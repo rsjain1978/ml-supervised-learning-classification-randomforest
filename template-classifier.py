ecda77742ab0b129222ff5ecef9b11157d270b0c
rsjain1978 <rsjain1978@gmail.com>
2019-02-04 23:19:20 +0530

    template classifier file

---
+++ template-classifier.py
@@ -0,0 +1,10 @@
+
+#Acquire the data in an accessible format
+#Identify and correct missing data points/anomalies as required
+#Prepare the data for the machine learning model
+#Establish a baseline model that you aim to exceed
+#Train the model on the training data
+#Make predictions on the test data
+#Compare predictions to the known test set targets and calculate performance metrics
+#If performance is not satisfactory, adjust the model, acquire more data, or try a different modeling technique
+#Interpret model and report results visually and numerically
